d8ca6e5140bfc6256003fa8159fbb77caa806c50
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-03-14 18:25:51 +0200

    add avatars styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import RegistrationScreen from "./assets/screens/auth/RegistrationScreen";
-import LoginScreen from "./assets/screens/auth/LoginScreen";
+import RegistrationScreen from "./assets/screens/RegistrationScreen";
+import LoginScreen from "./assets/screens/LoginScreen";
 import { useFonts } from "expo-font";
 import { useCallback } from "react";
 import * as SplashScreen from "expo-splash-screen";
@@ -8,10 +8,5 @@ import { View } from "react-native";
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-    <>
-      <RegistrationScreen />
-      {/* <LoginScreen /> */}
-    </>
-  );
+  return <>{true ? <RegistrationScreen /> : <LoginScreen />}</>;
 }
